fd0dd5563210a69957f0a69c3fab62ead253ebc4
aman-4321 <amanclash4321@gmail.com>
2024-09-30 12:16:05 +0530

    fixed width issue

--- frontend/src/components/Quote.tsx
+++ frontend/src/components/Quote.tsx
@@ -1,6 +1,6 @@
 export const Quote = () => {
   return (
-    <div className="bg-slate-200 h-screen flex justify-center flex-col">
+    <div className="bg-slate-200 h-screen flex justify-center flex-col w-full">
       <div className="flex  justify-center">
         <div className="max-w-lg  ">
           <div className="text-3xl font-bold">
