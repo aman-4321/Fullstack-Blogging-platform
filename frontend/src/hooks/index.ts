eba758d5ce3fe664658dba0af72c5b6c106d3a86
aman-4321 <amanclash4321@gmail.com>
2025-02-07 21:19:17 +0530

    update blogs authorization

--- frontend/src/hooks/index.ts
+++ frontend/src/hooks/index.ts
@@ -16,16 +16,10 @@ export const useBlog = ({ id }: { id: string }) => {
   const [blog, setBlog] = useState<Blog>();
 
   useEffect(() => {
-    axios
-      .get(`${BACKEND_URL}/api/v1/blog/${id}`, {
-        headers: {
-          Authorization: localStorage.getItem("token"),
-        },
-      })
-      .then((response) => {
-        setBlog(response.data.blog);
-        setLoading(false);
-      });
+    axios.get(`${BACKEND_URL}/api/v1/blog/${id}`).then((response) => {
+      setBlog(response.data.blog);
+      setLoading(false);
+    });
   }, [id]);
 
   return {
@@ -39,16 +33,10 @@ export const useBlogs = () => {
   const [blogs, setBlogs] = useState<Blog[]>([]);
 
   useEffect(() => {
-    axios
-      .get(`${BACKEND_URL}/api/v1/blog/bulk`, {
-        headers: {
-          Authorization: localStorage.getItem("token"),
-        },
-      })
-      .then((response) => {
-        setBlogs(response.data.blogs);
-        setLoading(false);
-      });
+    axios.get(`${BACKEND_URL}/api/v1/blog/bulk`).then((response) => {
+      setBlogs(response.data.blogs);
+      setLoading(false);
+    });
   }, []);
 
   return {
